cdbe564166892884436633bcd94dd334d5a365f0
Jiahan Chen <57107814+JCHENHAN54@users.noreply.github.com>
2022-12-12 15:53:28 +0100

    Delete stuhomeAPP directory

--- stuhomeAPP/src/main/java/com/stuhome/app/dao/ContactDao.java
+++
@@ -1,8 +0,0 @@
-package com.stuhome.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.stuhome.app.entity.Contact;
-
-public interface ContactDao extends JpaRepository<Contact, Long>{
-	
-}
--- stuhomeAPP/src/main/java/com/stuhome/app/dao/ImgDao.java
+++
@@ -1,8 +0,0 @@
-package com.stuhome.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.stuhome.app.entity.Img;
-
-public interface ImgDao extends JpaRepository<Img, Long>{
-	
-}
